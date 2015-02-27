99ae6b58e4c1f06e29d3f7668c3678ac7ed82bb7
Bucky Schwarz <d.w.schwarz@gmail.com>
2015-02-27 16:26:21 -0500

    drew some cool shit

--- js/game_controller.js
+++ js/game_controller.js
@@ -8,13 +8,24 @@ var GameController = Class.extend({
 
     // game world updates go here 
     update: function() {
-
         requestAnimationFrame(this.update.bind(this));
         this.render();
     },
 
     // rendering calls go here 
     render: function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        
+        //this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawGrid();
+    },
+
+    drawGrid: function() {
+         for (var i = 0; i < this.canvas.width; i+= 5) {
+                this.context.beginPath();
+                this.context.moveTo(0,i);
+                this.context.lineTo(i, this.canvas.height);
+                this.context.fillColor = '#DDD';
+                this.context.stroke();
+         }
     }
 });
